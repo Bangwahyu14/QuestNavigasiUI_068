90e52a8aabcb1f952209877d1ccfc07980b1ec0d
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-06 13:32:28 +0700

    menambahkan beberapa import

--- app/src/main/java/com/example/navigation/view/Formulir.kt
+++ app/src/main/java/com/example/navigation/view/Formulir.kt
@@ -4,6 +4,8 @@ import android.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -13,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormIsian(
