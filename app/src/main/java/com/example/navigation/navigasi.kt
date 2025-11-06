73af429a12ab82c56a290d1ed41b5095a4719fb6
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-06 12:53:25 +0700

    menambahkan fungsi untuk button backto formulirku

--- app/src/main/java/com/example/navigation/navigasi.kt
+++ app/src/main/java/com/example/navigation/navigasi.kt
@@ -5,9 +5,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.compose.composable
+import com.example.navigation.view.FormIsian
+
 enum class Navigasi{
     Formulirku,
     Detail
@@ -22,8 +25,9 @@ fun DataApp(
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
+            modifier = Modifier.padding(isiRuang),
 
-            modifier = Modifier.padding(isiRuang)){
+        ){
             composable(route = Navigasi.Formulirku.name){
                 FormIsian (
                     //pilihanJK = JenisK .map { id -> konteks.resroude.getstring}
@@ -39,6 +43,12 @@ fun DataApp(
                     }
                 )
             }
+
         }
     }
+}
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+){
+
 }
