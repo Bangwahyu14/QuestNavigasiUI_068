540bcd631b301a61bf2de1c66aca70bffc845856
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-06 13:11:43 +0700

    menambahkan scaffold

--- app/src/main/java/com/example/navigation/view/Formulir.kt
+++ app/src/main/java/com/example/navigation/view/Formulir.kt
@@ -1,12 +1,20 @@
 package com.example.navigation.view
 
 import android.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
-    jenis:List<string> =listOf("Laki-laki","Perempuan"),
+    jenis:List<R.string> =listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick : () -> unit
 ){
+    Scaffold (modifier= Modifier,
+        topBar = {
+            TopAppBar(
 
+            )
+        })
 }
