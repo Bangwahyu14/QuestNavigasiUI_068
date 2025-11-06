adadf1d559593baad897a6f941d75b2003031d9f
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-06 13:09:46 +0700

    menambahkan form isian di file Formulir.kt

---
+++ app/src/main/java/com/example/navigation/view/Formulir.kt
@@ -0,0 +1,12 @@
+package com.example.navigation.view
+
+import android.R
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenis:List<string> =listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> unit
+){
+
+}
