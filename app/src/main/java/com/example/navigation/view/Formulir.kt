35aa32cec15266f915bf3c7a12eb640a636b709d
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-06 13:25:37 +0700

    memperbaiki katayang eror

--- app/src/main/java/com/example/navigation/view/Formulir.kt
+++ app/src/main/java/com/example/navigation/view/Formulir.kt
@@ -2,19 +2,30 @@ package com.example.navigation.view
 
 import android.R
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun FormIsian(
     jenis:List<R.string> =listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
-    Scaffold (modifier= Modifier,
-        topBar = {
+    Scaffold (modifier = Modifier,
+        {
             TopAppBar(
+                title = { Text(stringResource(id= R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (colorResource(id = R.color.teal_700))
+            )}
+    ){ isiRuang ->
 
-            )
-        })
+
+    }
 }
