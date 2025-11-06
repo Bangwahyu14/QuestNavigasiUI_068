8521d816995209ae9b15759f09172de6c2646977
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-06 12:53:56 +0700

    menambahkan screen tujuan agar tetap di backstage

--- app/src/main/java/com/example/navigation/navigasi.kt
+++ app/src/main/java/com/example/navigation/navigasi.kt
@@ -50,5 +50,6 @@ fun DataApp(
 private fun cancelAndBackToFormulirku(
     navController: NavHostController
 ){
-
+    navController.popBackStack(Navigasi.Formulirku.name,
+        inclusive = false)
 }
