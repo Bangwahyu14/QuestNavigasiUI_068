08cb1d81d6b603366cad54835c22db1740006697
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-06 12:10:21 +0700

    menambahkan file navigation.kt, menambahkan enum class,membuat fungsi dataapp

--- app/src/main/java/com/example/navigation/navigasi.kt
+++ app/src/main/java/com/example/navigation/navigasi.kt
@@ -1,6 +1,22 @@
 package com.example.navigation
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
 enum class navigasi{
     formulirku,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang ->
+
+    }
 }
